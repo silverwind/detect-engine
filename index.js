2d80f4b161db216a450c0e912e02f7e05d83c3af
silverwind <me@silverwind.io>
2015-08-07 23:14:13 +0200

    Update some things

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ if (process.release) {
   module.exports = process.release.name === "io.js" ? "iojs" : "node";
 } else {
   var engine = require("path").basename(process.execPath).toLowerCase();
-  module.exports = process.platform === "win32" ? engine.replace(/\.exe$/,"") : engine;
+  module.exports = process.platform === "win32" ? engine.replace(/\.exe$/, "") : engine;
 }
